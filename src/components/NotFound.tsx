633d71b134ca042763f93c8d4683d0f5ce9b721b
Purin <purin.peera@mail.kmutt.ac.th>
2025-05-18 06:32:50 +0700

    NotFound page

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+
+const NotFound = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/")
+  }
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flex-row items-center justify-center gap-1">
+        <MagnifyingGlassIcon className="w-10 h-10 text-gray-400" /> 
+        <p className="text-3xl text-gray-400">Not Found</p>
+      </div>
+      <p className="text-gray-400">
+        We couldn’t find the Pokemon you're searching for.
+      </p>
+      <button onClick={() => handleClick()} className="mt-4 cursor-pointer bg-white shadow-md p-2 rounded-xl hover:shadow-lg hover:bg-gray-50 transition-all duration-300 text-black">
+        Try another name
+      </button>
+    </div>
+  );
+};
+
+export default NotFound;
