70745b14c86ce33d9eb173ea7465f442823efa22
Purin <purin.peera@mail.kmutt.ac.th>
2025-05-19 00:47:21 +0700

    add suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,26 @@
-"use client"
+"use client";
 import AllPokemon from "@/components/AllPokemon";
 import Search from "@/components/Search";
-import { useState } from "react";
+import { useState, Suspense } from "react";
 
 const Home = () => {
-  const [searchValue, setSearchValue] = useState("")
+  const [searchValue, setSearchValue] = useState("");
 
   return (
-      <main className="flex flex-col w-full max-w-7xl mx-auto px-5 bg-white shadow-md min-h-screen">
-        <div className="flex flex-col items-center">
-          <p className="text-5xl mt-5">PokeSource</p>
-        </div>
-        <div>
-          <Search searchValue={searchValue} setSearchValue={setSearchValue}/>
-        </div>
-        <div className="mb-5">
-          <AllPokemon searchValue={searchValue}/>
-        </div>
-      </main>
+    <main className="flex flex-col w-full max-w-7xl mx-auto px-5 bg-white shadow-md min-h-screen">
+      <div className="flex flex-col items-center">
+        <p className="text-5xl mt-5">PokeSource</p>
+      </div>
+      <div>
+        <Suspense fallback={<p>Loading Search...</p>}>
+          <Search searchValue={searchValue} setSearchValue={setSearchValue} />
+        </Suspense>
+      </div>
+      <div className="mb-5">
+        <AllPokemon searchValue={searchValue} />
+      </div>
+    </main>
   );
-}
+};
 
 export default Home;
