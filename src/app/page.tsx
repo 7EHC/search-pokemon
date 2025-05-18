4a1b0c96a59d4b394b3944c711e2ebc88e100a7b
Purin <purin.peera@mail.kmutt.ac.th>
2025-05-19 01:30:38 +0700

    edit loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ const Home = () => {
       <div>
         <Suspense
           fallback={
-            <div className="flex justifycenter animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-black"></div>
+            <div className="flex justify-center animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-black"></div>
           }
         >
           <Search searchValue={searchValue} setSearchValue={setSearchValue} />
