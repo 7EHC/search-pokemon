e5159cdb5f488b8045aefd7906399462379bfe87
Purin <purin.peera@mail.kmutt.ac.th>
2025-05-20 11:21:05 +0700

    add new pokeball icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ const Home = () => {
   return (
     <main className="flex flex-col w-full max-w-7xl mx-auto px-5 bg-white shadow-md min-h-screen">
       <div className="flex justify-center">
-        <p className="text-3xl mt-5 font-semibold md:text-5xl">
-          Pokemon Source
+        <p className="flex text-3xl mt-5 font-semibold md:text-5xl">
+          P<span className="flex items-end"><img src="/pokeball.svg" alt="pokeball" className="md:w-9 w-6.5"/></span>kedex
         </p>
       </div>
       <div>
