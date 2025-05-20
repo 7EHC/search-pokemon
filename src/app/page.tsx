628a9823c4e992d74ce1fb17509359f130a6063b
Purin <purin.peera@mail.kmutt.ac.th>
2025-05-20 11:35:02 +0700

    change home page top logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,9 @@ const Home = () => {
   const [searchValue, setSearchValue] = useState("");
 
   return (
-    <main className="flex flex-col w-full max-w-7xl mx-auto px-5 bg-white shadow-md min-h-screen">
+    <main className="flex flex-col w-full max-w-7xl mx-auto px-5 py-5 bg-white shadow-md min-h-screen">
       <div className="flex justify-center">
-        <p className="flex text-3xl mt-5 font-semibold md:text-5xl">
-          P<span className="flex items-end"><img src="/pokeball.svg" alt="pokeball" className="md:w-9 w-6.5"/></span>kedex
-        </p>
+        <img src="/pokedex.png" alt="pokedex" className="md:w-60 w-50"/>
       </div>
       <div>
         <Suspense>
